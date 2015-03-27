224a5cd8c2576c0e41b4ad5396986b5407dffa33
Alexey Dubnyak <adubnyak@gmail.com>
2015-03-27 18:05:50 +0200

    index page final cut (fucking piece of shit)

--- js/main.js
+++ js/main.js
@@ -22,6 +22,8 @@ $(document).ready(function(){
     $('#collapse_xs_menu_on').on('click', function(e){
         e.preventDefault();
         $('#collapse_xs_menu').toggle();
+        $('.overlay-menu').toggle();
+
     });
 
 });
